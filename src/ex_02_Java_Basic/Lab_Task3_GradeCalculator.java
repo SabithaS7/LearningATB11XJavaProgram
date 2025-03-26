b919015bf6be1cbc36a74485079ffd72acf368b7
Sabtesting <130066900+Sabtesting@users.noreply.github.com>
2025-03-26 11:50:47 +0530

    Changes in Task code

--- src/ex_02_Java_Basic/Lab_Task3_GradeCalculator.java
+++ src/ex_02_Java_Basic/Lab_Task3_GradeCalculator.java
@@ -1,10 +1,20 @@
 package ex_02_Java_Basic;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Lab_Task3_GradeCalculator {
     public static void main(String[] args) {
-        int score=90;
-        char Grade = (score<=100 && score>=90)?'A':(score<90 && score>=80)?'B':
+       // A: 90-100 B: 80-89 C: 70-79 D: 60-69 F: 0-59
+        //int score=90;
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Score: ");
+        int score=sc.nextInt();
+
+      char Grade = (score<=100 && score>=90)?'A':(score<90 && score>=80)?'B':
                 (score<80 && score>=70)?'C':(score<70 && score>=60)?'E':'F';
         System.out.println("Your grade is "+Grade);
+
     }
 }
